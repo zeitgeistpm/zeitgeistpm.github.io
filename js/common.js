77a0901a304f4172866c2e07d08db0cbb8afb9f1
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-09-08 12:57:11 +0200

    format all

--- js/common.js
+++ js/common.js
@@ -2,81 +2,84 @@ document.addEventListener("click", eventDocClick, false);
 window.addEventListener("load", WindowLoad, false);
 
 function WindowLoad() {
-	if(window.innerWidth >= 1200) {
-		var r;
-		if(r = document.querySelector(".headerPage__wrapImg .rellax")) {
-			var itr = 0;
-			var interval = setInterval(function () {
-				itr++;
-				if(r.clientHeight) {
-					clearInterval(interval);
-					loadJS('https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js', callRellax, document.body);
-					r.classList.add("active");
-				}
-				if(itr >= 10) {
-					clearInterval(interval);
-					r.classList.add("active");
-				}
-			}, 300);
-		}
-	}
+  if (window.innerWidth >= 1200) {
+    var r;
+    if ((r = document.querySelector(".headerPage__wrapImg .rellax"))) {
+      var itr = 0;
+      var interval = setInterval(function () {
+        itr++;
+        if (r.clientHeight) {
+          clearInterval(interval);
+          loadJS(
+            "https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js",
+            callRellax,
+            document.body
+          );
+          r.classList.add("active");
+        }
+        if (itr >= 10) {
+          clearInterval(interval);
+          r.classList.add("active");
+        }
+      }, 300);
+    }
+  }
 }
 function eventDocClick(e) {
-    var targ = e.target;
-    var clickedEl = e.target;
+  var targ = e.target;
+  var clickedEl = e.target;
 
-    while (targ && targ != this) {
-    	if (targ.classList.contains("headerMain__menu__bar")) {
-    		targ.classList.toggle("active");
-    		var menu;
-    		if(targ.classList.contains("active")) {    			
-	    		if(menu = document.querySelector(".headerMain__menu")) {
-	    			menu.classList.add("active");
-	    			document.body.classList.add("headerMainMenuActive")
-	    		}
-    		}
-    		else {
-    			if(menu = document.querySelector(".headerMain__menu")) {
-	    			menu.classList.remove("active");
-	    			document.body.classList.remove("headerMainMenuActive");
-	    		}	
-    		}
-    		break;
-    	}
-        targ = targ.parentNode;
+  while (targ && targ != this) {
+    if (targ.classList.contains("headerMain__menu__bar")) {
+      targ.classList.toggle("active");
+      var menu;
+      if (targ.classList.contains("active")) {
+        if ((menu = document.querySelector(".headerMain__menu"))) {
+          menu.classList.add("active");
+          document.body.classList.add("headerMainMenuActive");
+        }
+      } else {
+        if ((menu = document.querySelector(".headerMain__menu"))) {
+          menu.classList.remove("active");
+          document.body.classList.remove("headerMainMenuActive");
+        }
+      }
+      break;
     }
+    targ = targ.parentNode;
+  }
 }
 
-var fnDelay = function () {
-    var timer = 0;
-    return function (callback, ms) {
-        clearTimeout(timer);
-        timer = setTimeout(callback, ms);
-    };
-}();
-var loadJS = function(url, callback, locToInsert){
-    var scriptTag = document.createElement('script');
-    scriptTag.src = url;
+var fnDelay = (function () {
+  var timer = 0;
+  return function (callback, ms) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+var loadJS = function (url, callback, locToInsert) {
+  var scriptTag = document.createElement("script");
+  scriptTag.src = url;
 
-    scriptTag.onload = callback;
-    scriptTag.onreadystatechange = callback;
+  scriptTag.onload = callback;
+  scriptTag.onreadystatechange = callback;
 
-    locToInsert.appendChild(scriptTag);
+  locToInsert.appendChild(scriptTag);
 };
-var callRellax = function(){
-	var rellax = new Rellax('.rellax', {
-		center: true,
-		zIndex:-1
-	});
-}
+var callRellax = function () {
+  var rellax = new Rellax(".rellax", {
+    center: true,
+    zIndex: -1,
+  });
+};
 AOS.init({
-	/*disable: function () {
+  /*disable: function () {
 		var maxWidth = 1024;
 		return window.innerWidth < maxWidth;
 	},*/
-	offset: 100,
-	duration: 3000,
-	// easing: 'ease-in-sine',
-	// delay: 100,
-	// once: true
+  offset: 100,
+  duration: 3000,
+  // easing: 'ease-in-sine',
+  // delay: 100,
+  // once: true
 });
